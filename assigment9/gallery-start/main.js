fa778404165b1b10787cd0a5646989be566baf84
AlexanderHernacki <112111557+AlexanderHernacki@users.noreply.github.com>
2022-10-23 14:22:46 -0600

    debug

--- assigment9/gallery-start/main.js
+++ assigment9/gallery-start/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images=['images/pic1.png','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
+const images=['images/pic1.PNG','images/pic2.jpg','images/pic3.jpg','images/pic4.jpg','images/pic5.jpg'];
 /* Declaring the alternative text for each image file */
 //const imageText{pic1:"an image of something",pic2:"an image of something",pic3:"an image of something",pic4:"an image of something",pic5:"an image of something"};
 const imageText=['an image of something','an image of something','an image of something','an image of something','an image of something'];
